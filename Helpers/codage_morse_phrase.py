a921ac1b8297b0002e26df966058ed0f32dacb2e
Jean-Marc Alcaïno <74672672+JMAlcaino@users.noreply.github.com>
2024-04-03 19:15:08 +0200

    Update codage_morse_phrase.py

--- Helpers/codage_morse_phrase.py
+++ Helpers/codage_morse_phrase.py
@@ -58,17 +58,17 @@ morse_code = {
        '@':[1,2,2,1,2,1],
        ' ':[1,1,1,1,1,1,1]}
 
-phr=input("entrez un mot ou phrase...")  # entrée de la phrase
+phr=input("entrez un mot ou phrase...")                # entrée de la phrase
 
 # codage
-indice=0 # indice juste pour voir si on lit bien la bonne lettre
-for ltr in phr:  # boucle pour chaque lettre 'ltr' dans la phrase 'phr'
-    if ltr in morse_code: # boucle de recherche du caractère dans le dictionnaire 'morse_code'
-        code_list = morse_code.get(ltr)     # récupère la liste qui est en valeur de la clé 'ltr'
-    print (ltr,indice,code_list)  # écrit la lettre 'ltr' son 'indice' dans la phrase 'phr' et la liste associée 'code_list'
-    code_list = [1,1,1] # pour le temps de pause entre deux lettre en morse --> 0.75" soient 3 '.'  Attention c'est une pause pas des bips (time.sleep)
+indice=0                                               # indice juste pour voir si on lit bien la bonne lettre
+for ltr in phr:                                        # boucle pour chaque lettre 'ltr' dans la phrase 'phr'
+    if ltr in morse_code:                              # boucle de recherche du caractère dans le dictionnaire 'morse_code'
+        code_list = morse_code.get(ltr)                # récupère la liste qui est en valeur de la clé 'ltr'
+    print (ltr,indice,code_list)                       # écrit la lettre 'ltr' son 'indice' dans la phrase 'phr' et la liste associée 'code_list'
+    code_list = [1,1,1]                                # pour le temps de pause entre deux lettre en morse --> 0.75" soient 3 '.'  Attention c'est une pause pas des bips (time.sleep)
     print (code_list) 
-    indice+=1 # pour la boucle des indices
+    indice+=1                                          # pour la boucle des indices
         
     
   
